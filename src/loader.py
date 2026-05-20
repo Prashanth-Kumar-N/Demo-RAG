e6394fe7152558beff8ee4cd34160808a2b9c524
Prashanth Kumar <70272376+Prashanth-Kumar-N@users.noreply.github.com>
2026-05-20 08:29:32 +0000

    loading initial commit message template...

--- src/loader.py
+++ src/loader.py
@@ -1,1 +1,20 @@
-from pymu
+import langchain
+from langchain_community.document_loaders import PyMuPDFLoader
+
+print("langchain", langchain.__version__)
+
+files = [
+    "./data/3_JLG_Boom_lifts_Catalog.pdf",
+]
+
+
+docs = []
+for file in files:
+    loader = PyMuPDFLoader(file)
+    pdf = loader.load()
+    docs = pdf
+    for doc in docs:
+        doc.metadata["name"] = file.split("/")[-1] 
+
+print(len(docs))
+print(docs[1].page_content);
